a52e00a8e63862625b296122f0e616f4811d1d20
Melanie Archer <job@twobanjos.com>
2020-06-24 16:16:51 -0700

    Visual refinements - column flex layout on mobile - border on submit button - remove excess borders on flex items - specify CSS concat order for build

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,14 @@ gulp.task('default', function() {
 });
 
 gulp.task('css', function() {
-  return gulp.src('./css/*.css')
+  return gulp.src(
+  ['./css/normalize.css', 
+  './css/main.css',
+  './css/header.css',
+  './css/footer.css',
+  './css/home.css',
+  './css/additional-content.css', 
+  './css/print.css'])
              .pipe(concat('all.css'))
              .pipe(gulp.dest('./dist/css'));
 });
