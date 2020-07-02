fba158bf65d7a96ed585f350137ac6c492c6ef5a
Melanie Archer <job@twobanjos.com>
2020-07-02 16:25:56 -0700

    Icon switching for mobile and table view

--- js/main.js
+++ js/main.js
@@ -1,20 +1,37 @@
-let pageNav = document.querySelector('.page-nav');
-let actionsNav = document.querySelector('.actions-nav');
-let primaryNav = document.querySelector('.primary-nav');
-let bars = document.querySelector('.page-nav .fa-bars');
-let times = document.querySelector('.page-nav .fa-times');
+let pageNav = document.querySelector(".page-nav");
+let actionsNav = document.querySelector(".actions-nav");
+let primaryNav = document.querySelector(".primary-nav");
+let primaryBars = document.querySelectorAll(".page-nav .fa-bars")[1];
+let times = document.querySelector(".page-nav .fa-times");
+let dropdownAngle = document.querySelectorAll(".page-nav .fa-angle-right");
 
-const toggleNav = (icon, target) => {
-  icon.addEventListener('click', (e) => {
-    console.log(`icon: ${icon}. target classList: ${target.classList}`);
-    target.classList.toggle('sr-only');
-    e.preventDefault();
+const toggleNav = (target) => {
+  target.style.display = target.style.display === "none" ? "" : "block";
+};
+
+const switchIcon = (iconClass, newIconClass) => {
+  let currentIcon = event.srcElement;
+  currentIcon.classList.remove(iconClass);
+  currentIcon.classList.add(newIconClass);
+};
+
+primaryBars.addEventListener("click", () => {
+  toggleNav(primaryNav);
+  switchIcon("fa-bars", "fa-times");
+});
+
+if (times) {
+  times.addEventListener('click', () => {
+    toggleNav(primaryNav);
+    switchIcon('fa-times', 'fa-bars');
   });
-  // change out bars to closer
-  // add border to pageNav
 }
-// add to page
-window.addEventListener('load', () => {
-  toggleNav(times, actionsNav);
-  // toggleNav(bars, primaryNav);
-})
+for (let angle of dropdownAngle) {
+  angle.addEventListener("click", () => {
+    if (angle.classList.contains("fa-angle-right")) {
+      switchIcon("fa-angle-right", "fa-angle-down");
+    } else {
+      switchIcon("fa-angle-down", "fa-angle-right");
+    }
+  });
+}
