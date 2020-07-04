50520e1a669b9a08130db35cc84501b7e1922783
Melanie Archer <job@twobanjos.com>
2020-07-03 17:54:53 -0700

    TODO: mobile; hover on tablet; hover on laptop

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 let pageNav = document.querySelector(".page-nav");
+let actionsNav = document.querySelector(".actions-nav");
+let primaryNav = document.querySelector(".primary-nav");
+
 // icons that only show in mobile view
 // let pageNavBars = document.querySelectorAll('.page-nav .fa-bars')[0];
 let pageNavCloser = document.getElementById("pageNavCloser");
-
-let actionsNav = document.querySelector(".actions-nav");
-let primaryNav = document.querySelector(".primary-nav");
 
 // icons that only show in mobile and tablet views
 // let primaryBars = document.querySelectorAll('.page-nav .fa-bars')[1];
 let primaryCloser = document.getElementById("primaryNavCloser");
 let dropdownAngle = document.querySelectorAll(".page-nav .fa-angle-right");
 
-const toggleNav = (target) => {
-  target.classList.toggle('visible');
+const toggleNav = (target, container) => {
+  target.classList.toggle("visible");
+  document.querySelector(container).classList.toggle('bordered');
 };
 
 const switchIcon = (iconClass, newIconClass) => {
@@ -21,18 +22,17 @@ const switchIcon = (iconClass, newIconClass) => {
 };
 
 primaryNavCloser.addEventListener("click", () => {
-  toggleNav(primaryNav);
-  primaryNavCloser.classList.toggle('fa-times');
+  toggleNav(primaryNav, '#primaryContainer');
+  primaryNavCloser.classList.toggle("fa-times");
 });
 
 pageNavCloser.addEventListener("click", () => {
-  toggleNav(pageNav);
-  switchIcon("fa-bars", "fa-times");
+  toggleNav(pageNav, '.page-nav');
+  pageNavCloser.classList.toggle('fa-times');
 });
 
 for (let angle of dropdownAngle) {
-  let submenu = angle.parentElement.querySelector('.submenu');
-  console.log(submenu);
+  let submenu = angle.parentElement.querySelector(".submenu");
 
   angle.addEventListener("click", () => {
     if (angle.classList.contains("fa-angle-right")) {
@@ -40,7 +40,8 @@ for (let angle of dropdownAngle) {
     } else {
       switchIcon("fa-angle-down", "fa-angle-right");
     }
-    submenu.classList.toggle('visible');
+    if (submenu) {
+      submenu.classList.toggle("visible");
+    }
   });
-
 }
