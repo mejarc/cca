7c1e48d15a5c51ad15e7b09e9c0fa74d12686ca4
Melanie Archer <job@twobanjos.com>
2020-07-05 15:52:48 -0700

    All views on the nav. Refactored show/hide. TODO: hover state debug

--- js/main.js
+++ js/main.js
@@ -1,45 +1,38 @@
 let pageNav = document.querySelector(".page-nav");
 let actionsNav = document.querySelector(".actions-nav");
 let primaryNav = document.querySelector(".primary-nav");
+let menuTriggers = document.querySelectorAll('.menu-trigger');
 
-// icons that only show in mobile view
-// let pageNavBars = document.querySelectorAll('.page-nav .fa-bars')[0];
+// icon that shows only in mobile view
 let pageNavCloser = document.getElementById("pageNavCloser");
 
 // icons that only show in mobile and tablet views
-// let primaryBars = document.querySelectorAll('.page-nav .fa-bars')[1];
 let primaryCloser = document.getElementById("primaryNavCloser");
 let dropdownAngle = document.querySelectorAll(".page-nav .fa-angle-right");
 
 const toggleNav = (target, container) => {
   target.classList.toggle("visible");
-  document.querySelector(container).classList.toggle('bordered');
+  if (container) {
+    document.querySelector(container).classList.toggle('bordered');
+  }
 };
 
-const switchIcon = (iconClass, newIconClass) => {
-  let currentIcon = event.srcElement;
-  currentIcon.classList.replace(iconClass, newIconClass);
-};
+pageNavCloser.addEventListener("click", () => {
+  toggleNav(actionsNav, '#pageNavContainer');
+  toggleNav(primaryNav, null);
+  pageNavCloser.classList.toggle('fa-times');
+});
 
 primaryNavCloser.addEventListener("click", () => {
   toggleNav(primaryNav, '#primaryContainer');
   primaryNavCloser.classList.toggle("fa-times");
 });
 
-pageNavCloser.addEventListener("click", () => {
-  toggleNav(pageNav, '.page-nav');
-  pageNavCloser.classList.toggle('fa-times');
-});
-
-for (let angle of dropdownAngle) {
-  let submenu = angle.parentElement.querySelector(".submenu");
-
-  angle.addEventListener("click", () => {
-    if (angle.classList.contains("fa-angle-right")) {
-      switchIcon("fa-angle-right", "fa-angle-down");
-    } else {
-      switchIcon("fa-angle-down", "fa-angle-right");
-    }
+for (let link of menuTriggers) {
+  let submenu = link.parentElement.querySelector(".submenu");
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    link.classList.toggle('open');
     if (submenu) {
       submenu.classList.toggle("visible");
     }
