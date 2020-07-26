5a1fdf1052e3310bdfa9fd7f98e499f6de1b4daf
Melanie Archer <job@twobanjos.com>
2020-07-26 15:24:38 -0700

    More restoration of mobile/tablet naviation, footer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 let pageNav = document.querySelector(".page-nav");
 let actionsNav = document.querySelector(".actions-nav");
+let primaryNav = document.querySelector(".primary-nav");
 let menuTriggers = document.querySelectorAll(".menu-trigger");
 
 // icon that shows only in mobile view
 let pageNavCloser = document.getElementById("pageNavCloser");
+
+// icons that only show in mobile and tablet views
+let primaryCloser = document.getElementById("primaryNavCloser");
 let dropdownAngle = document.querySelectorAll(".page-nav .fa-angle-right");
 
 const toggleNav = (target, container) => {
